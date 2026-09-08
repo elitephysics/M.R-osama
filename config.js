ad43d8125a329c5378c7881e35c828d2ab9f7b9b
elitephysics <elmohsenamr@gmail.com>
2026-09-09 00:17:34 +0300

    Add files via upload

--- config.js
+++ config.js
@@ -1,3 +1,3 @@
 // Public Supabase configuration
-const SUPABASE_URL = 'https://gwjgunpwyfleedvulesa.supabase.co';
-const SUPABASE_PUBLISHABLE_KEY = 'sb_publishable_BbqKmvkpAL8ONwtWoafB2Q_HrgIsnaz';
+const SUPABASE_URL = 'https://kxqpijjbratihawkctzn.supabase.co';
+const SUPABASE_PUBLISHABLE_KEY = 'sb_publishable_oLC6-zzQu9CNXwUCkXD76g_eO5ZhEmU';
